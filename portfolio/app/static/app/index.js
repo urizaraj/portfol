30288b91f6d33f5609399a6fdd1861bc175151ba
Alexander Urizar <urizaraj@gmail.com>
2018-01-22 11:25:02 -0500

    HTML tweaks

--- portfolio/app/static/app/index.js
+++ portfolio/app/static/app/index.js
@@ -1,16 +1,19 @@
 $(document).ready(function () {
     $(".container").slideDown(200)
 
-    $("#linkedin,#github,#email").on("mouseenter mouseleave", function(){
-        $(this).clearQueue().toggleClass("blue-background", {duration: 100, children: true})
+    $("#linkedin,#github,#email").on("mouseenter mouseleave", function () {
+        $(this).clearQueue().toggleClass("blue-background", {
+            duration: 100,
+            children: true
+        })
     })
 
-    $("#pheader").on("click", function(){
+    $("#pheader").on("click", function () {
         $("#projects").slideToggle(200)
         $("#arrow").toggleClass("fa-angle-down fa-angle-up")
     })
 
-    $(".right-border").on("mouseenter mouseleave", function(){
+    $(".right-border").on("mouseenter mouseleave", function () {
         $(this).clearQueue().toggleClass("right-border-thick", 100)
     })
 });
