d3f627c7d1b0a96dcd8d2dec3cfcc76ae8284b46
Alexander Urizar <urizaraj@gmail.com>
2018-02-06 12:11:23 -0500

    Homepage tweaks.

--- portfolio/app/static/app/js/index.js
+++ portfolio/app/static/app/js/index.js
@@ -1,6 +1,7 @@
 let projects = $("#projects")
 let about = $("#about")
 let header = $("#aboutheader,#pheader")
+let arrow = $("#arrow,#arrow-2")
 
 $(document).ready(function () {
     $(".container").slideDown(200)
@@ -8,6 +9,6 @@ $(document).ready(function () {
     header.on("click", function () {
         projects.slideToggle(200)
         about.slideToggle(200)
-        $("#arrow,#arrow-2").toggleClass("fa-angle-down fa-angle-up")
+        arrow.toggleClass("fa-angle-down fa-angle-up")
     })
 });
